e3823d3cbc7f9ca99e1890e22a9a2193bb3968ee
Uyiosa <hiskonxeptz@gmail.com>
2023-07-03 19:54:25 +0200

    fix build

--- backend/build.sh
+++ backend/build.sh
@@ -2,16 +2,16 @@
 set -o errexit
 
 echo "Creating a virtual env for both poetry and our packages..."
-python -m venv venv
+python -m venv .venv
 
 echo "Updating pip ;-)..."
-./venv/bin/python -m pip install --upgrade pip
+./.venv/bin/python -m pip install --upgrade pip
 
 echo "Installing new/better version of poetry into our virtual env..."
-./venv/bin/pip install poetry==1.4.2
+./.venv/bin/pip install poetry==1.4.2
 
 echo "Installing our (non-dev) packages..."
 # cd /opt/render/project/backend
-./venv/bin/poetry install
+./.venv/bin/poetry install
 
 echo "Done"
